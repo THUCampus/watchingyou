2abbbb02ba48580c41bf11538beb5813d150af26
Richard <zhoucy16@163.com>
2018-09-12 00:23:33 +0800

    修复了本地资源不删除的bug

--- WatchingYou/cctv/models.py
+++ WatchingYou/cctv/models.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+
 class User(models.Model):
     name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
@@ -15,3 +18,6 @@ class Image(models.Model):
     add_time = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(upload_to='images/')
 
+@receiver(pre_delete, sender=Image)
+def mymodel_delete(sender, instance, **kwargs):
+    instance.img.delete(False)
