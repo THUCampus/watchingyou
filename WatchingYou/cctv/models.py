7f734d7235a414834df567f4f40a131526e2519b
Richard <zhoucy16@163.com>
2018-09-12 18:58:02 +0800

    写好了账号系统的运行逻辑
    用户数据库
    超级用户
    修改密码
    超级用户添加新用户
    注销
    看监控
    串场

--- WatchingYou/cctv/models.py
+++ WatchingYou/cctv/models.py
@@ -1,22 +1,44 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
-
+from django.utils import timezone
 from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
+from django.contrib.auth.hashers import make_password, check_password
 
 class User(models.Model):
     name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
     isSuperUser = models.BooleanField(default=False)
+
+    password_en = models.BooleanField(default=False)
+
+    encrypt_items = ['password']
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        for attr in self.encrypt_items:
+            if not getattr(self, '%s_en' % attr):
+                self.__setattr__(attr, make_password(getattr(self, attr)))
+                self.__setattr__('%s_en' % attr, True)
+        super(User, self).save(*args, **kwargs)
+
+    def is_password_right(self, password):
+        return check_password(password, self.password)
+
+
 class Image(models.Model):
     add_time = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return str(self.add_time)
+
+    def was_load_recently(self):
+        return self.add_time > timezone.now() - datetime.timedelta(seconds=2)
 
 @receiver(pre_delete, sender=Image)
 def mymodel_delete(sender, instance, **kwargs):
--- WatchingYou/cctv/views.py
+++ WatchingYou/cctv/views.py
@@ -7,60 +7,244 @@ from django.http import HttpResponse
 
 from .models import User, Image
 
-from django.core.signing import Signer
-
-signer = Signer()
-
 def index(request):
-    if not request.session.get('login_user_exists'):
-        request.session['login_user_exists'] = True
-    if not request.session.get('login_password_right'):
-        request.session['login_password_right'] = True
     if request.session.get('login'):
-        return redirect('/video/')
-    context = {
-        'user_exists': request.session['login_user_exists'],
-        'password_right': request.session['login_password_right'],
-    }
-    return render(request, 'cctv/index.html', context)
+        return redirect('/cctv/menu/')
+
+    if request.method == 'GET':
+        context = {
+            'user_exists': True,
+            'password_right': True,
+        }
+        return render(request, 'cctv/index.html', context)
+    elif request.method == 'POST':
+        print("post")
+        context = {
+            'user_exists': True,
+            'password_right': True,
+        }
+        return render(request, 'cctv/index.html', context)
 
 def login_check(request):
+    if request.session.get('login'):
+        return redirect('/cctv/menu/')
+
     if request.method == 'POST':
-        user = User.objects.filter(name=request.POST['user_name'])
-        password = request.POST['user_password']
+        login_user_exists = True
+        login_password_right = True
+        user = User.objects.filter(name=request.POST['login_user_name'])
+        password = request.POST['login_user_password']
+
         if not user:
-            request.session['login_user_exists'] = False
-            return redirect('/cctv/')
-        if user[0].password != signer.sign(password):
-            request.session['login_password_right'] = False
-            return redirect('/cctv/')
-        if user[0].isSuperUser:
-            request.session['login_is_super_user'] = True
+            login_user_exists = False
+        elif not user[0].is_password_right(password):
+            login_password_right = False
+
+        if login_user_exists and login_password_right:
+            if request.session.get('login'):
+                del request.session['login']
+            request.session['login'] = user[0].name
+            request.session['isSuperUser'] = user[0].isSuperUser
+            return redirect('/cctv/menu/')
         else:
-            request.session['login_is_super_user'] = False
-        if request.session.get('login'):
-            del request.session['login']
-        request.session['login'] = user[0]
-        return redirect('/video/')
+            print('fail')
+            context = {
+                'user_exists': login_user_exists,
+                'password_right': login_password_right,
+            }
+            return render(request, 'cctv/index.html', context)
+    elif request.method == 'GET':
+        return redirect('/cctv/')
+
+def menu(request):
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+
+    if request.method == 'POST':
+        print('post_to_menu')
+        return
+    elif request.method == 'GET':
+        context = {
+            'user': request.session.get('login'),
+            'isSuperUser': request.session.get('isSuperUser'),
+        }
+        return render(request, 'cctv/menu.html', context)
 
 def register(request):
-    return
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+    if not request.session.get('isSuperUser'):
+        return redirect('/cctv/menu/')
+
+    if request.method == 'POST':
+        print('post_to_settings')
+        return
+    elif request.method == 'GET':
+        context = {
+            'user_existed': False,
+            'user_illegal': False,
+            'password_illegal': False,
+            'password_different': False,
+        }
+        return render(request, 'cctv/register.html', context)
 
 def register_check(request):
-    return
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+    if not request.session.get('isSuperUser'):
+        return redirect('/cctv/menu/')
+
+    if request.method == 'POST':
+        user_existed = False
+        user_illegal = False
+        password_illegal = False
+        password_different = False
+        user_name = request.POST['user_name']
+        user_password = request.POST['user_password']
+        user_password_confirm = request.POST['user_password_confirm']
+
+        if not user_name:
+            user_illegal = True
+        elif len(user_name) > 20:
+            user_illegal = True
+        elif User.objects.filter(name=user_name):
+            user_existed = True
+        if not user_password:
+            password_illegal = True
+        elif len(user_password) > 20:
+            password_illegal = True
+        elif user_password != user_password_confirm:
+            password_different = True
+
+        if user_existed or user_illegal or password_illegal or password_different:
+            context = {
+                'user_existed': user_existed,
+                'user_illegal': user_illegal,
+                'password_illegal': password_illegal,
+                'password_different': password_different,
+            }
+            return render(request, 'cctv/register.html', context)
+        else:
+            new_user = User(name=user_name, password=user_password)
+            new_user.save()
+            context = {
+                'title': u'注册成功',
+                'message': u'新用户注册成功',
+                'destination': '/cctv/menu/',
+            }
+            return render(request, 'cctv/message.html', context)
+    elif request.method == 'GET':
+        return redirect('/cctv/register/')
+
+def settings(request):
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+
+    if request.method == 'POST':
+        print('post_to_settings')
+        return
+    elif request.method == 'GET':
+        context = {
+            'user': request.session.get('login'),
+            'old_password_right': True,
+            'new_password_illegal': False,
+        }
+        return render(request, 'cctv/settings.html', context)
+
+def settings_check(request):
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+
+    if request.method == 'POST':
+        user = User.objects.filter(name=request.session.get('login'))
+        password = request.POST['old_user_password']
+        new_password = request.POST['new_user_password']
+        if not user:
+            del request.session['login']
+            context = {
+                'title': u'错误',
+                'message': u'用户不存在，请重新登陆',
+                'destination': '/cctv/',
+            }
+            return render(request, 'cctv/message.html', context)
+        elif not user[0].is_password_right(password):
+            context = {
+                'user': request.session.get('login'),
+                'old_password_right': False,
+                'new_password_illegal': False,
+            }
+            return render(request, 'cctv/settings.html', context)
+        if not new_password:
+            context = {
+                'user': request.session.get('login'),
+                'old_password_right': True,
+                'new_password_illegal': True,
+            }
+            return render(request, 'cctv/settings.html', context)
+        elif len(new_password) > 20:
+            context = {
+                'user': request.session.get('login'),
+                'old_password_right': True,
+                'new_password_illegal': True,
+            }
+            return render(request, 'cctv/settings.html', context)
+        else:
+            user[0].password = new_password
+            user[0].password_en = False
+            user[0].save()
+            del request.session['login']
+            context = {
+                'title': u'修改成功',
+                'message': u'密码修改成功，请重新登陆',
+                'destination': '/cctv/',
+            }
+            return render(request, 'cctv/message.html', context)
+    elif request.method == 'GET':
+        return redirect('/cctv/settings/')
+
 
 def video(request):
-    imgs = Image.objects.all().order_by('add_time')
-    context = {
-        'img': imgs[0],
-    }
-    return render(request, 'cctv/video.html', context)
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+
+    if request.method == 'POST':
+        print('post_to_video')
+        return
+    elif request.method == 'GET':
+        imgs = Image.objects.all().order_by('add_time')
+        if not imgs:
+            return render(request, 'cctv/video.html', {})
+        context = {
+            'img': imgs[0],
+        }
+        return render(request, 'cctv/video.html', context)
 
 def video_refresh(request):
-    imgs = Image.objects.all().order_by('add_time')
-    imgs[0].delete()
-    img_str = str(imgs[1].img)
-    response = HttpResponse()
-    response['Content-Type'] = "text/plain"
-    response.write(img_str)
-    return response
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+
+    if request.method == 'POST':
+        print('post_to_video_fresh')
+        return
+    elif request.method == 'GET':
+        response = HttpResponse()
+        response['Content-Type'] = "text/plain"
+        imgs = Image.objects.all().order_by('add_time')
+        if not imgs:
+            return response
+        img_str = str(imgs[0].img)
+        response.write(img_str)
+        return response
+
+def logout(request):
+    if not request.session.get('login'):
+        return redirect('/cctv/')
+
+    if request.method == 'POST':
+        del request.session['login']
+        request.session['isSuperUser'] = False
+        return  redirect('/cctv/')
+    elif request.method == 'GET':
+        del request.session['login']
+        request.session['isSuperUser'] = False
+        return redirect('/cctv/')
